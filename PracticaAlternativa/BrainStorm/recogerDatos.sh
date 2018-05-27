2ade6016f447cea4586d7d5820b1e98e30213ce5
isega24 <isega24@hotmail.com>
2018-05-27 19:58:01 +0000

    :(

--- PracticaAlternativa/BrainStorm/recogerDatos.sh
+++ PracticaAlternativa/BrainStorm/recogerDatos.sh
@@ -2,7 +2,9 @@
 
 for j in `seq 0 19`; do
     for i in `seq 10 20 50`; do
-        ./main.py $j $i 20 20000 
+        rm ./solucionesDimension$i/funcion$j.sol
+        echo "" > ./solucionesDimension$i/funcion$j.sol
+        ./main.py $j $i 100 0
 
     done
 done
